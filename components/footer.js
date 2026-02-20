bfb39fb4e5fc9a7e8974df0dd83365c9b537a531
Dara <daraantonioa@gmail.com>
2026-02-19 20:35:40 -0600

    modificaciones 1

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+export function renderFooter() {
+    document.body.innerHTML += `
+        <footer class="bg-dark text-white text-center py-3 mt-5">
+            Sistema IoT Puertas Residenciales <br>
+            Desarrollado por Dara A.
+        </footer>
+    `;
+}
